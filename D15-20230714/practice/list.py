380bb27ca6fc23d5e6b54607f78dc0086aa751f6
supriya212003 <supriyaiyyappan2003@gmail.com>
2023-07-22 12:54:37 +0530

    karka

---
+++ D15-20230714/practice/list.py
@@ -0,0 +1,18 @@
+# names=[{"name":"vajeeha","place":"thittuvilai","hobbies":["music","drama","movies"]},
+#      {"name":"supriya","place":"ganesapuram","hobbies":["music","drawing","drama"]},
+#      {"name":"valli","place":"Krishnankovil","hobbies":['meme creating','music','drawing']},
+#      {"name":"srivarthanan","place":"swamithoppu"},
+#      {"name":"sreeram","place":"thamarai kulam"},
+#      {"name":"subin","place":"parvathipuram"},
+#      {"name":"robin","place":"tirunelveli"}]
+# bio=(names[2]["hobbies"])
+# # for i in bio:
+# #     print(i)
+
+
+ # bio={"name":"vaji","place":"thittuvilai","husband":"mr"}
+ # print(bio["husband"])
+marks={"tamil":100,"english":78,"maths":89,"science":67,"social":87}
+print(marks.items())
+print(marks.values()) 
+print(marks.keys())  

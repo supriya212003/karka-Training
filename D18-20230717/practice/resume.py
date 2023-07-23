c1bdbd406d7d1c3c4712d306d1ef052903f153b4
supriya212003 <supriyaiyyappan2003@gmail.com>
2023-07-23 10:26:50 +0530

    karka

--- D18-20230717/practice/resume.py
+++ D18-20230717/practice/resume.py
@@ -4,10 +4,10 @@ my_resume={
             "mob_no":6381943829,
             "soft_skills":["team work","adaptability"],
             "hard_skills":"coding",
-            "edu_qualification":{
-                "sslc":{"scl_name":"st.joseph convent higher seconday school","percentage":"80%"},
-                "hsc":{"scl_name":"st.joseph convent higher seconday school","percentage":"82%"},
-                "degree":{"clg_name":"govt.arts and science college","percentage":"73%"}},
+            "edu_qualification":[{
+                "study":"sslc","scl_name":"st.joseph convent higher seconday school","percentage":"80%"},
+                {"study":"hsc","scl_name":"st.joseph convent higher seconday school","percentage":"82%"},
+                {"study":"degree","clg_name":"govt.arts and science college","percentage":"73%"}],
             "project":"youtube transcript summarizer",
             "experience":[{"company name":"flow","role":"frontend developer","duration":1.2},
                          {"company name":"zoho","role":"teaching assistant","duration":2},
@@ -19,18 +19,27 @@ my_resume={
                 "languages_known":["tamil","english"],
                 "d.o.b":21-5-2003, 
                 "gender":"female",
-                "married_status":"single"           
-                },
-            "adress":{
+                "married_status":"single" ,          
+                "adress":{
                 "street name":"pulavarvilai",
                 "village":"ganesapuram",
                 "district":"kanyakumari"
-                }}
+                }}}
 
 
-# add=my_resume[adress]
-# for i in add:
-#     print(i["village"])
-for i in my_resume['personal_details']:
-    # print(i)
-    print(f"{i}:{my_resume['personal_details'][i]}")
+education=my_resume["edu_qualification"]
+for ed in education:
+    print(ed["study"])
+    print(ed["percentage"])
+
+add=my_resume["personal_details"]["adress"]
+for i in add:
+    print(add[i])
+
+lan=my_resume["personal_details"]["languages_known"]
+print(lan[1])
+
+ad=my_resume["personal_details"]["adress"]
+for ads in ad:
+    if ads=="village":
+        print(ad["village"])

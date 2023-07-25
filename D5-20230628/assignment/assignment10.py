655a3686dd93b0455311249f7f2eeaaf11c2e685
supriya212003 <supriyaiyyappan2003@gmail.com>
2023-07-25 12:26:10 +0530

    karka

---
+++ D5-20230628/assignment/assignment10.py
@@ -0,0 +1,6 @@
+b=24
+#base of the triangle
+h=33
+#height of the tringle
+print("area of triangle=",(24*33)/2)
+#area of triangle=(base*height)/2
---
+++ D5-20230628/assignment/assignment3.py
@@ -0,0 +1,6 @@
+print("supriya")
+#display my name
+print("a=9")
+#display a=9
+print(7*6)
+#display the above multiplication

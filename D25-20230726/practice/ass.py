825ea3a550b150ed84fcbbed8e2488f5a4c142e4
supriya212003 <supriyaiyyappan2003@gmail.com>
2023-07-27 21:02:02 +0530

    karka

---
+++ D25-20230726/practice/ass.py
@@ -0,0 +1,24 @@
+# tym=int(input("how many times to print:"))
+# for i in range(0,tym):
+    
+#     # print("*"*tym)
+#     for j in range():
+#         print("*",end=" ")
+#     print(" ")
+
+
+
+tym=int(input("how many times to print:"))
+
+for i in range((tym*tym),0,-1):
+    # print(i,end=" ")
+    if i%tym==0:
+        print(" ")
+    print(i,end=" ")
+    
+
+
+        
+
+
+    
